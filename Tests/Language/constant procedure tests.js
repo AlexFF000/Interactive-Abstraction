00fc27fe50a31a199c7669c38c8ac77aca47e53c
AlexFF000 <42391315+AlexFF000@users.noreply.github.com>
2021-07-25 18:02:57 +0100

    Planned some tests for the allocation procedure

--- Tests/Language/constant procedure tests.js
+++ Tests/Language/constant procedure tests.js
@@ -43,3 +43,51 @@ async function Base2ExponentProc_testAllValues(){
     // Test passed for all exponents
     return true;
 }
+
+/*
+    AllocationProc
+    Procedure for allocating memory
+*/
+
+// Tests for allocating on the int/float pool
+
+// Test1- Allocating when the pool is empty
+async function AllocationProc_testGlobalPoolAllocateWhenEmpty(){
+    // Use Eval stack to request a space on the int/float pool then run the procedure
+    /* Then afterwards check that:
+        a) The top value on the Eval stack contains the starting address of the pool
+        b) The PoolFreePointer points to the 6th byte of the pool
+        c) The first four bytes of the next space after the one allocated (i.e. the 6th to 10th bytes of the pool) contain 0 (showing that there is no previously deallocated space)
+    */
+}
+// Test2- Allocating when pool is not empty, but not full
+async function AllocationProc_testGlobalPoolAllocateWhenHalfFull(){
+    // Fill half of the free spaces in the pool, incrementing the PoolFreePointer accordingly
+    // Then use Eval stack to request a space and run the procedure
+    /* Then afterwards check that:
+        a) The top value on the Eval stack contains the starting address of the first block after those that have been filled
+        b) The PoolFreePointer points to the starting address of the next space
+        c) The first four bytes of the next space after the one allocated contain 0
+    */
+}
+// Test3- Trying to allocate when pool is full (it should allocate from the heap instead, and the pool should not be modified)
+async function AllocationProc_testGlobalPoolAllocateWhenFull(){
+    // Fill all of the free spaces in the pool, incrementing PoolFreePointer accordingly
+    // Then use Eval stack to request a space and run the procedure
+    /* Then afterwards check that:
+        a) The top value on the Eval stack contains an address, but not one from inside the pool
+        b) The PoolFreePointer has not been changed
+
+
+    */
+}
+// Test4- Reallocating previously deallocated space
+async function AllocationProc_testGlobalPoolReallocateDeallocated(){
+    // Fill half of the free spaces in the pool, then deallocate one of these spaces (but not one at the start or end)
+    // Then use Eval stack to request a space and run the procedure
+    /*
+        Then afterwards check that:
+            a) The top value on the Eval stack contains the starting address of the deallocated space
+            b) The PoolFreePointer points to the first space after the halfway point (a new space should not have been allocated)
+    */
+}
