b73203b1ad405884667ab80b571c0623529bc802
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-10-16 17:21:25 +0100

    NamePool.create now also clears pointer in first chunk

--- Language/Structures/NamePool.js
+++ Language/Structures/NamePool.js
@@ -3,6 +3,7 @@
 */
 class NamePool{
     static _headersLength = 7;
+    static _blockSize = 5;
 
     static create(instructionsLength){
         // Return instructions to create a new name pool in the current scope (this function only handles creating new pools, not expansion pools)
@@ -22,20 +23,29 @@ class NamePool{
             copy(Addresses.ps3, Addresses.ps1 + 1, 4)
         );
         instructs.push(
-            // Load the number of 5 byte blocks in the first free space into the 6th byte
+            // Load the number of blocks in the first free space into the 6th byte
             "AND 0",
-            `ADD ${Math.floor(runtime_options.NamePoolSize - this._headersLength / 5)}`,
+            `ADD ${Math.floor(runtime_options.NamePoolSize - this._headersLength / this._blockSize)}`,
             `WRT ${Addresses.ps2 + 1}`,
             // Load the number of expansion pools into the 7th byte (there are none yet, so this will be 0)
             "AND 0",
             `WRT ${Addresses.ps2 + 2}`
         );
+        // Also make sure the first free chunk's pointer to the next free chunk is clear, as it is the only free chunk
+        instructs = instructs.concat(
+            writeMultiByte(0, Addresses.ps2 + 3, 4),
+            // Also set its field for the number of blocks in the next free chunk to 0
+            [
+                "AND 0",
+                `WRT ${Addresses.ps3 + 3}`
+            ]
+        )
         // Copy headers to the pool
         instructs = instructs.concat(
             copy(poolAddressPointer, Addresses.psAddr, 4)
         );
         instructs = instructs.concat(
-            copyToAddress(Addresses.ps1, 7, instructionsLength + calculateInstructionsLength(instructs))
+            copyToAddress(Addresses.ps1, 12, instructionsLength + calculateInstructionsLength(instructs))
         )
         return instructs;
     }
