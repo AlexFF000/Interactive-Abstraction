28e874f4a988d55ed348ba89d540c8e37d20dc0d
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2022-04-10 17:55:32 +0100

    Implement test_DECLARE_globalVarTableWithForceGlobal

--- Tests/Language/data function tests.js
+++ Tests/Language/data function tests.js
@@ -38,7 +38,31 @@ async function test_DECLARE_globalVarTableWithoutForceGlobal(){
     return assertMemoryEqualToInt(Addresses.NullAddress, firstSlot + 6, 4);
 }
 // Test2- Add variable to the global variable table, with the forceGlobal flag
-async function test_DECLARE_globalVarTableWithForceGlobal(){return "NOT IMPLEMENTED";
+async function test_DECLARE_globalVarTableWithForceGlobal(){
+    /*
+        Declare a variable called "testVar" and check that:
+            a) Bytes 2-5 (the name address field) of the second slot (the first slot will already be the parent entry) in the global VarTable contains an address from the global name pool
+            b) The address pointed to by the name address field is a name containing "testVar"
+            c) Bytes 6-9 (the data pointer) of the second slot in the global VarTable contains null
+    */
+    await runSetup(declareTests_neededSetup);
+    let globalVarTable = readMemoryAsInt(Addresses.GlobalArea + Offsets.frame.VarTablePointer, 4);
+    let globalNamePool = readMemoryAsInt(Addresses.GlobalArea + Offsets.frame.NamePoolPointer, 4);
+    let name = new Name("testVar");
+    let modifiers = new Modifiers();
+    modifiers.setGlobal(true);
+    let parameters = [name, modifiers];
+    await runInstructions(DECLARE(parameters, testsInstructionsStart), false, true);
+    let firstSlot = globalVarTable + VarTable._parentHeadersLength + VarTable._entryLength;
+    // Check a
+    let nameAddress = readMemoryAsInt(firstSlot + 2, 4);
+    if (!(globalNamePool <= nameAddress && nameAddress < globalNamePool + runtime_options.NamePoolSize)) return "Assertion Error: Value is not in expected range";
+    // Check b
+    for (let i = 0; i < name.bytes.length; i++){
+        if (readMemoryAsInt(nameAddress + i, 1) !== name.bytes[i]) return "Assertion Error: Values do not match";
+    }
+    // Check c
+    return assertMemoryEqualToInt(Addresses.NullAddress, firstSlot + 6, 4);
 }
 // Test3- Add variable to a local variable table
 async function test_DECLARE_localVarTable(){
